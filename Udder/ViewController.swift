fb7ff8d893738839a3205f03a8e4854164b6f474
Anjali Singh <anjali.singh@comviva.com>
2021-05-22 23:50:19 +0530

    Add view will disappear.

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -25,7 +25,12 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         //
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        //
+    }
 
 
 }
 
+    
